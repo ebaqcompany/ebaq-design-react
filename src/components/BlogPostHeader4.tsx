14f1319d24224ed178fb2862d8c0d972eff4fd94
ebaqcompany <ebaqcompany@gmail.com>
2026-08-19 19:00:16 -0400

    Match blog hero width to article content

--- src/components/BlogPostHeader4.tsx
+++ src/components/BlogPostHeader4.tsx
@@ -14,7 +14,7 @@ export const BlogPostHeader4 = (props: BlogPostHeader4Props) => {
   return (
     <section className="px-[5%] py-8 md:py-12 lg:py-16">
       <div className="container">
-        <div className="grid gap-x-20 gap-y-16 md:gap-y-20 xl:grid-cols-2">
+        <div className="grid gap-x-20 gap-y-16 md:gap-y-20 xl:grid-cols-[0.5fr_1fr]">
           <div className="relative z-10 mx-auto flex size-full max-w-lg flex-col items-start justify-start">
             <div className="mb-5 flex w-full items-center justify-start md:mb-6">
               <p className="rounded-full border border-black/20 px-3 py-1 text-sm font-normal">{category}</p>
